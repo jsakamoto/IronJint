1d5379607fb116d89ca59f5e4637e3846b255b57
jsakamoto <junichi.jp@gmail.com>
2015-04-08 18:48:43 +0900

    1st commit.

---
+++ IronJint/JavaScriptCode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Jint;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Runtime;
+
+namespace IronJint
+{
+    /// <summary>
+    /// JavaScriptCode
+    /// </summary>
+    public class JavaScriptCode : ScriptCode
+    {
+        /// <summary>
+        /// Gets or sets the jint engine.
+        /// </summary>
+        /// <value>
+        /// The jint engine.
+        /// </value>
+        public Engine JintEngine { get; protected set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JavaScriptCode"/> class.
+        /// </summary>
+        /// <param name="sourceUnit">The source unit.</param>
+        public JavaScriptCode(SourceUnit sourceUnit)
+            : base(sourceUnit)
+        {
+            JintEngine = new Engine();
+        }
+
+        /// <summary>
+        /// Runs the specified scope.
+        /// </summary>
+        /// <param name="scope">The scope.</param>
+        /// <returns></returns>
+        public override object Run(Scope scope)
+        {
+            var scopeStrage = (ScopeStorage)scope.Storage;
+            foreach (var item in scopeStrage.GetItems())
+            {
+                JintEngine.SetValue(item.Key, item.Value);
+            }
+
+            var code = this.SourceUnit.GetCode();
+            JintEngine.Execute(code);
+
+            return null;
+        }
+    }
+}
---
+++ IronJint/JavaScriptContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Runtime;
+
+namespace IronJint.Runtime
+{
+    /// <summary>
+    /// JavaScriptContext
+    /// </summary>
+    public class JavaScriptContext : LanguageContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JavaScriptContext"/> class.
+        /// </summary>
+        /// <param name="domainManager">The domain manager.</param>
+        /// <param name="options">The options.</param>
+        public JavaScriptContext(ScriptDomainManager domainManager, IDictionary<string, object> options)
+            : base(domainManager)
+        {
+        }
+
+        /// <summary>
+        /// Parses the source code within a specified compiler context.
+        /// The source unit to parse is held on by the context.
+        /// </summary>
+        /// <param name="sourceUnit"></param>
+        /// <param name="options"></param>
+        /// <param name="errorSink"></param>
+        /// <returns>
+        ///   <b>null</b> on failure.
+        /// </returns>
+        /// <remarks>
+        /// Could also set the code properties and line/file mappings on the source unit.
+        /// </remarks>
+        public override ScriptCode CompileSourceCode(SourceUnit sourceUnit, CompilerOptions options, ErrorSink errorSink)
+        {
+            return new JavaScriptCode(sourceUnit);
+        }
+    }
+}
